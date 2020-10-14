ef990bdf728e0459bb4b1827dcbf25f4cba72af3
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-14 16:22:45 +0000

    this program will tell the user what their score is based off of the correct answer and the student answer

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-/**
+import java.util.Scanner;
+/** this program will ask for the correct awnsers and then compare them to the awnsers the student provided giving them a score at the end.
  *
- * @author 
+ * @author Isaac
  */
 public class Main {
 
@@ -10,6 +11,29 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner (System.in);
+    System.out.println("Please enter the correct awnsers");
+    int[] awnsers = new int [5];
+    for(int i = 0; i < awnsers.length; i++){
+    awnsers[i] = input.nextInt();
+    }
+    System.out.println("Please enter the student awnsers");
+    int[] stuawns = new int [5];
+    for(int i = 0; i < stuawns.length; i++){
+      stuawns[i] = input.nextInt();
+    }    
+    int done = 0;
+     for(int i = 0; i < 5; i++){
+       if((stuawns[i]) == (awnsers[i])){
+         
+         
+       done = done + 1;
+         }
+     }
+       System.out.println("Score " + done);
+     
+    }
   }
-}
+
+  
+
